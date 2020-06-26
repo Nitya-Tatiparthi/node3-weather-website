8cb5fc586c63241b2d17fe4c2768c1fccc650681
unknown <nityasreerprn@gmail.com>
2020-06-26 09:01:52 +0530

    small changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,4 @@
 const request = require('request')
-const myURL = require('url')
-
 
 const forecast = (lat, long, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=0a6324252a3b2d5d0681f5c3c760846c&query=' + lat + ',' + long
@@ -12,8 +10,7 @@ const forecast = (lat, long, callback) => {
         } else if(body.error){
             callback('Unable to find location. Try another search')
         } else{
-            const icon = myURL.parse(body.current.weather_icons[0])
-            callback(undefined, body.current.weather_descriptions[0] + ' ' + icon + '\n. It is currently ' + body.current.temperature + ' degrees out. But it feels like ' + body.current.feelslike + '.')
+            callback(undefined, body.current.weather_descriptions[0] + '.'  + '\nIt is currently ' + body.current.temperature + ' degrees out. But it feels like ' + body.current.feelslike + '.')
         }
     })
 }
